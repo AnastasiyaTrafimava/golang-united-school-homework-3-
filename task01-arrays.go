eea0c8ef65ec5dc1c1f608aee6adab9131286a41
Anastasiya Trafimava <zAnastasiyaT@pttep.com>
2022-05-11 09:46:02 +0300

    lesson 03

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,10 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
-	return
+	var sum float32 = 0.0
+	for el := 0; el < len(input); el++ {
+		sum += input[el]
+	}
+
+	return sum / float32(len(input))
 }
--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,34 @@
 package homework
 
+import "sort"
+
+type Map struct {
+	key   int
+	value string
+}
+
+type ByIndex []Map
+
+func (ps ByIndex) Len() int {
+	return len(ps)
+}
+
+func (ps ByIndex) Less(k, m int) bool {
+	return ps[k].key < ps[m].key
+}
+
+func (ps ByIndex) Swap(k, j int) {
+	ps[k], ps[j] = ps[j], ps[k]
+}
+
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
+	m := make([]Map, 0)
+	for k, v := range input {
+		m = append(m, Map{k, v})
+	}
+	sort.Sort(ByIndex(m))
+	for _, v := range m {
+		result = append(result, v.value)
+	}
 	return
 }
